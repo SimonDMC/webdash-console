3430f723f729b0ea218ee3a4560781af71594ef0
SimonDMC <simondmcplayer@gmail.com>
2023-05-28 00:29:06 +0200

    Make ctrl scroll behave correctly

--- components/ZoomButtons.tsx
+++ components/ZoomButtons.tsx
@@ -1,5 +1,6 @@
 import { getZoomLevel, setZoomLevel } from "@/lib/LocalStorage";
 import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
 
 type ZoomButtonsProps = {
     rerender: Function;
@@ -7,14 +8,37 @@ type ZoomButtonsProps = {
 
 const ZoomButtons = ({ rerender }: ZoomButtonsProps) => {
     function zoomIn() {
-        setZoomLevel(getZoomLevel() + 0.1);
+        setZoomLevel(Math.min(getZoomLevel() + 0.1, 1.5));
         rerender();
     }
 
     function zoomOut() {
-        setZoomLevel(getZoomLevel() - 0.1);
+        setZoomLevel(Math.max(getZoomLevel() - 0.1, 0.4));
         rerender();
     }
+
+    const listener = (e: WheelEvent) => {
+        if (!e.ctrlKey) return;
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.deltaY > 0) {
+            zoomOut();
+        } else {
+            zoomIn();
+        }
+    };
+
+    document.addEventListener("wheel", listener, {
+        passive: false,
+    });
+
+    useEffect(() => {
+        return () => {
+            document.removeEventListener("wheel", listener);
+        };
+    });
 
     return (
         <div className={styles.zoomButtons}>
