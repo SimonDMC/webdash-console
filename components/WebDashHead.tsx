539e5579c966cb4a02f6d401fb2e8dc8cdf99e46
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2023-07-29 08:54:27 +0200

    Update embed image link

--- components/WebDashHead.tsx
+++ components/WebDashHead.tsx
@@ -8,7 +8,7 @@ export default function WebDashHead() {
             <meta content="A Web Dashboard for your Minecraft server" property="og:description" />
             <meta content="A Web Dashboard for your Minecraft server" name="description" />
             <meta content="%START_URL%" property="og:url" />
-            <meta content="https://simondmc.com/media/webdash-hero.png" property="og:image" />
+            <meta content="https://simondmc.com/media/imgs/covers/plugins/webdash-full.webp" property="og:image" />
             <meta content="#232c35" data-react-helmet="true" name="theme-color" />
             <meta name="twitter:card" content="summary_large_image" />
             <link rel="apple-touch-icon" href="%START_URL%/icon-192.png" />
